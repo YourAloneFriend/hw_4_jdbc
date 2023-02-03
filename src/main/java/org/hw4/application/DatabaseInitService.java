78666628fbdb459e682652d2ff4372527707f824
VasylStepanov <vasya202003@gmail.com>
2023-02-03 02:26:42 +0200

    Initial commit

---
+++ src/main/java/org/hw4/application/DatabaseInitService.java
@@ -0,0 +1,20 @@
+package org.hw4.application;
+
+import org.hw4.application.storage.DataSource;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.hw4.application.GetQueries.getQueries;
+
+public class DatabaseInitService {
+
+    public static void main(String[] args) {
+        try (Statement statement = DataSource.getConnection().createStatement()) {
+            for (String query : getQueries("src/sql/init_db.sql"))
+                statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
